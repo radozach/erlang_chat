9f6bb07740e88784fa1f4bdbfe2cbef5840a04f1
rado <zachar.radoslav@gmail.com>
2013-04-08 01:45:07 +0200

    worker is initializing automatically by mainserver when user is logged, change pids and main server save users pid

--- multichatworker.erl
+++ multichatworker.erl
@@ -1,23 +1,21 @@
 -module(multichatworker).
 -behaviour(gen_server).
--export([create/1,
+-author("Bc. Radoslav Zachar, Bc. Jakub Calik").
+-date("1.4.2013").
+-record(user, {nick, pid}).
+-record(room, {name, pid}).
+-record(worker, {mypid, mainpid, users}).
+-export([
+%		create/1,
 		sendtouser/3,
 		die/1	
 		]).
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
 
--record(user, {nick, pid}).
--record(room, {name, pid}).
--record(worker, {mypid, spid, users}).
-
--author("Bc. Radoslav Zachar, Bc. Jakub Calik").
--date("1.4.2013").
-
 %%% Client API
-create(ParentPid) -> 
-	{ok, MyPid} = gen_server:start_link(?MODULE, ParentPid, []),
-	MyPid.
-
+%create(ParentPid) -> 
+%	gen_server:start_link(?MODULE, ParentPid, []).
+	
 sendtouser(MyPid, ToNick, Msg) ->
 	% ToPid = getPid(ToNick), 
 	gen_server:call(MyPid, {message, MyPid, ToNick, Msg}).
@@ -26,7 +24,9 @@ die(MyPid) ->
 	gen_server:call(MyPid, terminate).
 
 %%% Server functions
-init(ParentPid) -> {ok, make_worker(0, ParentPid, [])}. %% no treatment of info here!
+init(ParentPid) ->
+	MyPid = self(), 
+	{ok, make_worker(MyPid, ParentPid, [])}. 
  
 % receive message
 handle_call({message, FromPid, ToPid, Msg}, _From, Worker) when ToPid =:= Worker#worker.mypid ->
@@ -63,4 +63,4 @@ code_change(_OldVsn, State, _Extra) ->
 
 %%% Private functions
 make_worker(MyPid, ServerPid, Users) ->
-	#worker{mypid=MyPid, spid=ServerPid, users=Users}.
+	#worker{mypid=MyPid, mainpid=ServerPid, users=Users}.
